aba54b7afb02882e1dcc3831b113f69892d22e6a
adammflax <adammflax@btopenworld.com>
2020-06-06 20:38:52 +0000

    Deploying to gh-pages from  @ ca986d44631057342977cfe2c8c6453ccd1bfe97 🚀

---
+++ bundle/codedoc-bundle.entry.js
@@ -0,0 +1,41 @@
+import { getRenderer } from '/home/runner/work/mud-blog/mud-blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/renderer.js';
+import { initJssCs } from '/home/runner/work/mud-blog/mud-blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/setup-jss.js';initJssCs();
+import { installTheme } from '/home/runner/work/mud-blog/mud-blog/.codedoc/content/theme.ts';installTheme();
+import { countCards } from '/home/runner/work/mud-blog/mud-blog/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/article-card/count-cards.js';countCards();
+import { codeSelection } from '/home/runner/work/mud-blog/mud-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/selection.js';codeSelection();
+import { sameLineLengthInCodes } from '/home/runner/work/mud-blog/mud-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/same-line-length.js';sameLineLengthInCodes();
+import { initHintBox } from '/home/runner/work/mud-blog/mud-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-hint/index.js';initHintBox();
+import { initCodeLineRef } from '/home/runner/work/mud-blog/mud-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-ref/index.js';initCodeLineRef();
+import { initSmartCopy } from '/home/runner/work/mud-blog/mud-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/smart-copy.js';initSmartCopy();
+import { copyHeadings } from '/home/runner/work/mud-blog/mud-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/heading/copy-headings.js';copyHeadings();
+import { contentNavHighlight } from '/home/runner/work/mud-blog/mud-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/contentnav/highlight.js';contentNavHighlight();
+import { loadDeferredIFrames } from '/home/runner/work/mud-blog/mud-blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/deferred-iframe.js';loadDeferredIFrames();
+import { smoothLoading } from '/home/runner/work/mud-blog/mud-blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/smooth-loading.js';smoothLoading();
+import { tocHighlight } from '/home/runner/work/mud-blog/mud-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toc-highlight.js';tocHighlight();
+import { postNavSearch } from '/home/runner/work/mud-blog/mud-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/search/post-nav/index.js';postNavSearch();
+import { ToCToggle } from '/home/runner/work/mud-blog/mud-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toggle/index.js';
+import { DarkModeSwitch } from '/home/runner/work/mud-blog/mud-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
+import { ConfigTransport } from '/home/runner/work/mud-blog/mud-blog/.codedoc/node_modules/@codedoc/core/dist/es6/transport/config.js';
+import { ArticleCard } from '/home/runner/work/mud-blog/mud-blog/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/article-card/index.js';
+import { Author } from '/home/runner/work/mud-blog/mud-blog/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/author/index.js';
+import { ToCPrevNext } from '/home/runner/work/mud-blog/mud-blog/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/prevnext/index.js';
+
+const components = {
+  '+cg2tCLtbpl15dNW9SC8MA==': ToCToggle,
+  'sclGPlBODZ/6oDs4/BbU3w==': DarkModeSwitch,
+  'ccdKb7AujVX6SNWqY9ipVw==': ConfigTransport,
+  'XljTMEmS2ZfGU9Is2UkhAQ==': ArticleCard,
+  'mVRiVA/Nd82TIKfBzlbMIg==': Author,
+  'ue+S1GlejTroFvMDUFVq9Q==': ToCPrevNext
+};
+
+const renderer = getRenderer();
+const ogtransport = window.__sdh_transport;
+window.__sdh_transport = function(id, hash, props) {
+  if (hash in components) {
+    const target = document.getElementById(id);
+    renderer.render(renderer.create(components[hash], props)).after(target);
+    target.remove();
+  }
+  else if (ogtransport) ogtransport(id, hash, props);
+}
